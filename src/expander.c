8772d54fb80c1f5960468943bed60470e54a3743
Mateusz <mateusz.wojtczak42@gmail.com>
2024-10-13 23:46:59 +0200

    expander cleanup

--- src/expander.c
+++ src/expander.c
@@ -1,72 +1,103 @@
 
 #include "../include/minishell.h"
 
-char *expand_variables(const char *str, t_shell *shell) {
-    char *result = ft_strdup("");
-    if (!result) {
-        perror("malloc");
-        return NULL;
-    }
-    
-    int i = 0;
-    while (str[i]) {
-        if (str[i] == '$') {
-            char *expansion = NULL;
+char	*expand_exit_code(t_shell *shell, int *i)
+{
+	char	*expansion;
 
-            if (str[i + 1] == '?') {
-                // Expand $? to the exit code
-                expansion = ft_itoa(shell->exit_code);
-                i += 2; // Skip over $?
-            } else if (ft_isalpha(str[i + 1]) || str[i + 1] == '_') {
-                // Expand $VAR (an environment variable)
-                int var_start = i + 1;
-                while (str[i + 1] && (ft_isalnum(str[i + 1]) || str[i + 1] == '_')) {
-                    i++;
-                }
-                size_t var_len = i + 1 - var_start;
+	expansion = ft_itoa(shell->exit_code);
+	if (!expansion)
+		perror("malloc");
+	*i += 2;
+	return (expansion);
+}
 
-                // Extract variable name
-                char *var_name = ft_substr(str, var_start, var_len);
-                if (!var_name) {
-                    perror("malloc");
-                    free(result);
-                    return NULL;
-                }
+char	*expand_variable(const char *str, int *i)
+{
+	int		var_start;
+	char	*var_name;
+	char	*expansion;
 
-                // Get value using getenv
-                expansion = getenv(var_name);
-                free(var_name);
+	var_start = *i + 1;
+	while (str[*i + 1] && (ft_isalnum(str[*i + 1]) || str[*i + 1] == '_'))
+		(*i)++;
+	var_name = ft_substr(str, var_start, *i + 1 - var_start);
+	if (!var_name)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	expansion = getenv(var_name);
+	free(var_name);
+	if (!expansion)
+		expansion = "";
+	(*i)++;
+	return (ft_strdup(expansion));
+}
 
-                if (!expansion) {
-                    expansion = ""; // If variable doesn't exist, expand to an empty string
-                }
-                i++; // Move past the variable name
-            } else {
-                // If $ is not followed by ?, alphabetic character, or _, treat it literally
-                expansion = "$";
-                i++;
-            }
+char	*append_char(char *result, char c)
+{
+	char	temp[2];
+	char	*new_result;
 
-            // Concatenate expansion to result
-            char *temp = ft_strjoin(result, expansion);
-            free(result);
-            if (!temp) {
-                perror("malloc");
-                return NULL;
-            }
-            result = temp;
-        } else {
-            // Append regular character
-            char *temp = ft_strncat_char(result, str[i]);
-            free(result);
-            if (!temp) {
-                perror("malloc");
-                return NULL;
-            }
-            result = temp;
-            i++;
-        }
-    }
+	temp[0] = c;
+	temp[1] = '\0';
+	new_result = ft_strjoin(result, temp);
+	free(result);
+	if (!new_result)
+		perror("malloc");
+	return (new_result);
+}
 
-    return result;
+char	*expand_variables(const char *str, t_shell *shell)
+{
+	char	*result;
+	char	*expansion;
+	char	*temp_result;
+	int		i;
+
+	result = ft_strdup("");
+	if (!result)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if (str[i + 1] == '?')
+				expansion = expand_exit_code(shell, &i);
+			else if (ft_isalpha(str[i + 1]) || str[i + 1] == '_')
+				expansion = expand_variable(str, &i);
+			else
+			{
+				expansion = ft_strdup("$");
+				i++;
+			}
+			if (!expansion)
+			{
+				free(result);
+				return (NULL);
+			}
+			temp_result = ft_strjoin(result, expansion);
+			free(result);
+			free(expansion);
+			if (!temp_result)
+			{
+				perror("malloc");
+				return (NULL);
+			}
+			result = temp_result;
+		}
+		else
+		{
+			result = append_char(result, str[i]);
+			if (!result)
+				return (NULL);
+			i++;
+		}
+	}
+	return (result);
 }
